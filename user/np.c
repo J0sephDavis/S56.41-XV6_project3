90327877c67b9734fdf6735c8fc9ecff1b2311fd
Joseph Davis <josephdavisjoey@gmail.com>
2023-11-30 10:37:44 -0500

    added user/np to test dereferencing nullpointers

---
+++ user/np.c
@@ -0,0 +1,13 @@
+#include "user.h"
+int main(void) {
+	int *p = 0;
+	printf(1, "hello [%x]\n", *p);
+//	printf(1, "stack: %p\n", &p);
+//	p = malloc(100);
+//	printf(1, "heap: %p\n", p);
+//	p = 0;
+//	printf(0, "%c\n", p);
+//	*p = 100;
+//	free(p);
+	exit();
+}
